a94d1b0e96f97eee33e79cf13b6c745b9d20c6b4
Vilmar Viimsalu <vilmar.viimsalu@tktk.ee>
2024-08-19 00:16:43 +0300

    week 7

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Enter a text");
+string userImput = Console.ReadLine();
+
+PrintAnyWord(userImput);
+
+static void PrintAnyWord(string anyString) //anyString - parameter
+{
+    anyString = anyString.ToUpper();
+    for (int i = 0; i < 5; i++)
+    { 
+        Console.WriteLine(anyString);
+    }
+}
